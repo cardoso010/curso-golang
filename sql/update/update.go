d0d00553dde68b8eee30e4d1de15208129d13fcd
gabriel <gabriel.cardoso010@gmail.com>
2018-02-21 18:13:55 -0300

    Sql

---
+++ sql/update/update.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:01101993@/cursogo")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	stmt, _ := db.Prepare("update usuarios set nome = ? where id = ?")
+
+	stmt.Exec("Uóxiton Istive", 1)
+	stmt.Exec("Teste12331", 2)
+
+	stmt2, _ := db.Prepare("delete from usuarios where id = ?")
+	stmt2.Exec(3)
+}
